83500a6ada2a87279456614fcd90f7d995dff1b8
allface017 <113155656+allface017@users.noreply.github.com>
2023-11-28 14:44:25 +0900

    1-3_commit

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -11,20 +11,33 @@ class MyScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('alien1', 'assets/alien1.png');
+        this.load.image('city', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'alien1');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'city');
+        
+        this.taro = this.physics.add.sprite(500, 350, 'taro');
+
+
+
+        this.taro_direction = 1;
+
+        this.taro.angle = 0
+
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
+  // プレイヤーの移動
+//     if (this.taro_direction == 1) {
+//         this.taro.setVelocityX(45);
+//         this.taro.setVelocityY(-45);
+//     } 
     }
 
 }
